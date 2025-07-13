583d92715eb879d62937b5ebf7d7aef5878f6d4e
MarianaGallego <marianagallego97@hotmail.com>
2025-07-13 14:31:02 -0500

    FUNCIONA. Clases separadas y apariencia from mejorada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,47 +33,68 @@ namespace teoriaDeLenguajesLaboratorio9
             this.txtEntrada = new System.Windows.Forms.TextBox();
             this.btnAnalizar = new System.Windows.Forms.Button();
             this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightPink;
             this.label1.Location = new System.Drawing.Point(43, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.Size = new System.Drawing.Size(380, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ingrese la cadena:";
-            //this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtEntrada
             // 
-            this.txtEntrada.Location = new System.Drawing.Point(71, 82);
+            this.txtEntrada.BackColor = System.Drawing.SystemColors.Window;
+            this.txtEntrada.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEntrada.Location = new System.Drawing.Point(220, 139);
             this.txtEntrada.Name = "txtEntrada";
-            this.txtEntrada.Size = new System.Drawing.Size(286, 26);
+            this.txtEntrada.Size = new System.Drawing.Size(337, 28);
             this.txtEntrada.TabIndex = 1;
             // 
             // btnAnalizar
             // 
-            this.btnAnalizar.Location = new System.Drawing.Point(396, 73);
+            this.btnAnalizar.BackColor = System.Drawing.Color.SlateGray;
+            this.btnAnalizar.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalizar.ForeColor = System.Drawing.Color.Black;
+            this.btnAnalizar.Location = new System.Drawing.Point(311, 224);
             this.btnAnalizar.Name = "btnAnalizar";
-            this.btnAnalizar.Size = new System.Drawing.Size(102, 45);
+            this.btnAnalizar.Size = new System.Drawing.Size(152, 59);
             this.btnAnalizar.TabIndex = 2;
             this.btnAnalizar.Text = "Analizar";
-            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.UseVisualStyleBackColor = false;
             this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
             // 
             // txtResultado
             // 
-            this.txtResultado.Location = new System.Drawing.Point(71, 155);
+            this.txtResultado.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(220, 343);
             this.txtResultado.Name = "txtResultado";
-            this.txtResultado.Size = new System.Drawing.Size(286, 26);
+            this.txtResultado.Size = new System.Drawing.Size(337, 28);
             this.txtResultado.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Pink;
+            this.label2.Location = new System.Drawing.Point(97, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtResultado);
             this.Controls.Add(this.btnAnalizar);
             this.Controls.Add(this.txtEntrada);
@@ -91,6 +112,7 @@ namespace teoriaDeLenguajesLaboratorio9
         private System.Windows.Forms.TextBox txtEntrada;
         private System.Windows.Forms.Button btnAnalizar;
         private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label2;
     }
 }
 
---
+++ Parser.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace teoriaDeLenguajesLaboratorio9
+{
+    public class Parser
+    {
+        private List<string> tokens;
+        private int pos = 0;
+
+        public bool Analizar(string entrada)
+        {
+            tokens = Tokenizar(entrada);
+            if (tokens == null) return false;
+
+            pos = 0;
+            return S() && pos == tokens.Count;
+        }
+
+        private List<string> Tokenizar(string input)
+        {
+            var resultado = new List<string>();
+            var patron = new Regex(@"\s*(var|[0-9]+|[=+\-*/()])\s*");
+            var matches = patron.Matches(input);
+            int total = 0;
+
+            foreach (Match m in matches)
+            {
+                resultado.Add(m.Groups[1].Value);
+                total += m.Length;
+            }
+
+            return total == input.Length ? resultado : null;
+        }
+
+        private string Actual => pos < tokens.Count ? tokens[pos] : null;
+
+        private bool Coincidir(string esperado)
+        {
+            if (Actual == esperado)
+            {
+                pos++;
+                return true;
+            }
+            return false;
+        }
+
+        private bool S()
+        {
+            return Coincidir("var") && Coincidir("=") && E();
+        }
+
+        private bool E()
+        {
+            if (!T()) return false;
+            while (Actual == "+" || Actual == "-")
+            {
+                pos++;
+                if (!T()) return false;
+            }
+            return true;
+        }
+
+        private bool T()
+        {
+            if (!F()) return false;
+            while (Actual == "*" || Actual == "/")
+            {
+                pos++;
+                if (!F()) return false;
+            }
+            return true;
+        }
+
+        private bool F()
+        {
+            if (Actual == "(")
+            {
+                pos++;
+                if (!E()) return false;
+                return Coincidir(")");
+            }
+            else if (int.TryParse(Actual, out _))
+            {
+                pos++;
+                return true;
+            }
+            return false;
+        }
+    }
+}
